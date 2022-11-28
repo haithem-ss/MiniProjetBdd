c495063c677d04645a022e8c5f807eb56aecb70b
haithem-ss <haithemhadji@gmail.com>
2022-11-28 21:30:42 +0100

    Established connection with the database

---
+++ Server/Config/database.js
@@ -0,0 +1,23 @@
+import neo4j from 'neo4j-driver'
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+
+export async function initDriver(uri, username, password) {
+        const driver = neo4j.driver(
+            uri,
+            neo4j.auth.basic(
+              username,
+              password
+            )
+          )
+          try{
+            await driver.verifyConnectivity()      
+            console.log("Connection established")
+          }catch(e){
+            console.log(e)
+            console.log("Could not connect to the database")
+
+          }
+        return driver
+      }
--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-//import .env
+import express from 'express'
+import cors from 'cors'
+import {  initDriver } from './Config/database.js'
+import * as dotenv from 'dotenv'
+
+
+
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+  dotenv.config();
 }
 
-//global variables (including app)
+const PORT = process.env.PORT;
 
-PORT = process.env.PORT;
-app = express();
+const app = express();
 app.use(cors());
 
-//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-//define GET / home request response
 app.get("/", function (req, res) {
   res.status(200).send("We are up and running");
 });
 
 app.listen(PORT, function () {
-  console.log(`server running on localhost:${PORT}`);
+  console.log(`Server running on localhost:${PORT}`);
 });
-//
 
-module.exports = app;
+//database connection
+const uri = `neo4j+s://${process.env.CONNECTION_URL}`;
+const user = process.env.DATABASE_USER;
+const password = process.env.PASSWORD;
+
+await initDriver(uri, user, password)
+
+
+export default  app;
