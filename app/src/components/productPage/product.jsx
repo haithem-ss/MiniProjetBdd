68f3304ba86ef494091f703ebc8e9e40bc9b2870
KamaleddineBOULENACHE <kamilokamalo@gmail.com>
2022-12-17 23:34:50 +0100

    product page

--- app/src/components/productPage/product.jsx
+++ app/src/components/productPage/product.jsx
@@ -1,31 +1,67 @@
-import React from "react";
 import "./product.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from "react";
+
 
 
 function Product() {
+  const [productSpecs]=useState({
+    title:"Jacket For Winter",
+    brand:"Timberland",
+    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, non ab hic facilis harum, quam deleniti et dignissimos ullam, tempora unde suscipit veniam eos quod numquam eaque ipsum porro fuga?",
+    price:(19000).toLocaleString('en-US'),
+    deliveryDestinations:56,
+    colors:['#8E8162','#5A5E43','#2B2B2B','#3D4255'],
+    availableQte:6
+    })
+    const [selectedColor,setSelectedColor]=useState(0);
+    const [selectedQte,setSelectedQte]=useState(0);
+    
+    const handleInc=()=>{
+      if (selectedQte<productSpecs.availableQte){
+          setSelectedQte(selectedQte+1);
+      }
+      
+
+    }
+    const handleDec=()=>{
+      if (selectedQte>0){
+        setSelectedQte(selectedQte-1);
+      
+    }}
+    const stock={
+      Color:"black"
+    }
+    if (selectedQte===productSpecs.availableQte){
+      stock.color="red"
+      
+    }
+   
+    
   return (
     <div className="full">
       <div className="picDiv"></div>
+      
       <div className="infoDiv">
           <div className="descriptionDiv">
-            <h1>Jacket For Winter</h1>
-            <h4>TimberLand</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, non ab hic facilis harum, quam deleniti et dignissimos ullam, tempora unde suscipit veniam eos quod numquam eaque ipsum porro fuga?</p>
+
+            <h1>{productSpecs.title}</h1>
+            <h4>{productSpecs.brand}</h4>
+            <p>{productSpecs.description}</p>
           </div>
           <div className="seperator"></div>
           <div className="priceDiv">
-            <h1>19,000 DA</h1>
-            <h6>Livraison Disponible ver 56 wilaya </h6>
+            <h1>{productSpecs.price} DA</h1>
+            <h6>Livraison Disponible vers {productSpecs.deliveryDestinations} wilaya </h6>
           </div>
           <div className="seperator"></div>
           <div className="colorDiv">
             <h1>Choose A color</h1>
             <div className="colorSelectors">
-              <button className="colorSelect"></button>
-              <button className="colorSelect"></button>
-              <button className="colorSelect"></button>
-              <button className="colorSelect"></button>
+              {productSpecs.colors.map((color,index)=>{
+                if (index===selectedColor){return <button className="colorSelect" style={{backgroundColor:color, border: "5px solid blue"}} onClick= {() => setSelectedColor(index) } ></button>}
+                else
+                return <button className="colorSelect" style={{backgroundColor:color}} onClick= {() => setSelectedColor(index) }></button>})}
+                <p>{selectedColor}</p>
             </div>
           </div>
           <div className="seperator"></div>
@@ -33,16 +69,20 @@ function Product() {
             <h1>Choose Quantity</h1>
             <div className="quantitySelector">
               <div className="quantiyValue">
-                  <button className="quantityDecrement">-</button>
-                  <input className="quantity" type="text" readOnly />
-                  <button className="quantityIncrement">+</button>
+                  <button className="quantityDecrement" onClick={handleDec} ><h1>-</h1></button>
+                  <input className="quantity" type="text" value={selectedQte} style={stock} readOnly />
+                  <button className="quantityIncrement" onClick={handleInc}><h1>+</h1></button>
 
               </div>
               <div className="quantityText">
-              <h4>Available Quantity:</h4>
-              <h3>06 pieces</h3>
+              <h4 style={stock}>Available Quantity:</h4>
+              <h3 style={stock}>{productSpecs.availableQte}</h3>
               </div>
             </div>
+          </div>
+          <div className="submitionDiv">
+            <button className="cartButton"><h1>Add To Cart</h1></button>
+            <button className="purchaseButton"><h1>Purchase</h1></button>
           </div>
       </div>
     </div>
